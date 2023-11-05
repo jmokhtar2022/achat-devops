5e594a7cb7169b8aa4b1de90bd389dc940748812
IhebKraiem <iheb.kraiem@esprit.tn>
2023-11-06 00:36:32 +0100

    Junit test

---
+++ src/test/java/tn/esprit/rh/achat/FactureServiceImplJUnit.java
@@ -0,0 +1,44 @@
+package tn.esprit.rh.achat;
+
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.rh.achat.services.IFactureService;
+import tn.esprit.rh.achat.entities.Facture;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Date;
+import java.util.List;
+
+@SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
+public class FactureServiceImplJUnit {
+
+    @Autowired
+    IFactureService factureService;
+
+    @Test
+    @Order(1)
+    public void testRetrieveAllUsers() {
+        List<Facture> listUsers = factureService.retrieveAllFactures();
+        Assertions.assertEquals(2, listUsers.size());
+    }
+
+    @Test
+    @Order(2)
+    public void AddFacture() {
+            Facture facture = new Facture(100.0f, 500.0f, new Date(), new Date(), false);
+            factureService.addFacture(facture);
+            assertThat(facture.getIdFacture()).isGreaterThan(0);
+    }
+    @Test
+    @Order(3)
+    public void getFacture() {
+        Facture facture = factureService.retrieveFacture(1L);
+
+        assertThat(facture.getIdFacture()).isEqualTo(1L);
+    }
+
+
+}
